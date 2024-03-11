e7ad1759f2eb67c73edc703a9354fc9c9bda3c80
Nick Perez <home@Nicks-MacBook-Pro.local>
2024-03-11 16:56:30 -0700

    fixed arrow"

---
+++ build/precache-manifest.4b8b9c7b8bd76aa5ea6968c57e24d866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860d5aefe01c1763f02834c8046efec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c8edf5d9866ada2e01",
+    "url": "/static/css/2.3491d0c2.chunk.css"
+  },
+  {
+    "revision": "ce91751099eec4a60dfe",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "82c8edf5d9866ada2e01",
+    "url": "/static/js/2.ee9435be.chunk.js"
+  },
+  {
+    "revision": "ce91751099eec4a60dfe",
+    "url": "/static/js/main.8363e562.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
